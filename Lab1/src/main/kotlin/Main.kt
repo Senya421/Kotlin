6a6f43eae5394ecfebf808795396a22640a55980
Senya421 <90621550+Senya421@users.noreply.github.com>
2022-05-12 10:09:27 +0700

    Add files via upload

---
+++ Lab1/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var tree = BinatyTree()
+    tree.InsertNode(4)
+    tree.InsertNode(2)
+    tree.InsertNode(3)
+    tree.InsertNode(4)
+    tree.InsertNode(5)
+    tree.InsertNode(6)
+
+    println("Вывод полного дерева")
+    println(tree.printNode(tree.root))
+
+    println("Дерево после удаления элементов")
+    println(tree.DeleteNode(4))
+    println(tree.DeleteNode(6))
+    println(tree.printNode(tree.root))
+
+    println("Проверка на наличие элементов")
+    println(tree.containsNode(tree.root, 3))
+
+
+
+}
---
+++ Lab1/src/main/kotlin/MobilePhone.kt
@@ -0,0 +1,60 @@
+data class Contact(var name : String = "name", var number: String = "number" )
+
+
+class MobilePhone(val phonenumber: String, val contactlist : MutableList<Contact> = mutableListOf<Contact>()){
+
+    fun addNewContact(NewPhoneContact: Contact = Contact("name", "number")){
+        contactlist.add(NewPhoneContact)
+    }
+
+    fun updateContact(OldUpdatePhoneContact: Contact = Contact("name", "number"),
+                      NewPhoneContact : Contact = Contact("name", "number") ): Int {
+        var objCount = 0// Номер экземпляра объкета
+        var answCode = -1// Если он существует в списке контактов
+        for (contacts in contactlist){
+            if ((contacts == (OldUpdatePhoneContact)) && (objCount == 0)){
+                contacts.name = NewPhoneContact.name
+                contacts.number = NewPhoneContact.number
+                objCount = 0
+                answCode = 1
+            }
+        }
+        return answCode
+    }
+
+    fun removeContact(RemovedContact : Contact = Contact("name", "number")) : Int{
+        if (contactlist.contains(RemovedContact)){
+            println("Контакт успешно удален " + contactlist.remove(RemovedContact))
+            return 0 // существует
+        }
+        else{
+            println("Контакт не удален " + contactlist.remove(RemovedContact))
+            return -1 // Не существует
+        }
+
+    }
+
+    fun findContact(PhoneContact: Contact = Contact("name", "number")): Int {
+        if (contactlist.contains(PhoneContact)){
+            return contactlist.indexOf(PhoneContact)
+        }
+        else {
+            return -1
+        }
+    }
+
+    fun queryContact(findquery : String): String {
+        var question = "null"
+        for (item in contactlist){
+            if (findquery == item.name && question == "null" ){
+                question = item.toString()
+            }
+            else{
+                return continue
+
+            }
+        }
+        return question
+    }
+}
+
